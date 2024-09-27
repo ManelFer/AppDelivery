8424ce4c51295cba6526e8f4c7794b7aa4c93ebf
Manoel Ferreira Matos <manoelferreiramatos08@gmail.com>
2024-09-27 15:20:18 -0300

    Alteração
    15:19 27/09/2024
    Feita a criação do front do banner da aplicação

--- App/src/app/index.tsx
+++ App/src/app/index.tsx
@@ -1,15 +1,22 @@
-import { Text, View } from "react-native";
+import { Text, View, ScrollView } from "react-native";
+import { Header } from "../components/header";
+
+import Constants from "expo-constants";
+import { Banner } from "../components/banner";
+
+const statusBarHeight = Constants.statusBarHeight;
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <ScrollView
+      style={{ flex: 1 }}
+      className="bg-slate-200"
+      showsVerticalScrollIndicator={false}
     >
-      <Text className="text-red-700">Ola mundo.</Text>
-    </View>
+      <View className="w-full px-4" style={{ marginTop: statusBarHeight + 8 }}>
+        <Header />
+        <Banner />
+      </View>
+    </ScrollView>
   );
 }
