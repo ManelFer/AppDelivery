9d7af3336b04b63e05afcc45b75079ae89cd66fd
Manoel Ferreira Matos <manoelferreiramatos08@gmail.com>
2024-10-01 11:02:39 -0300

    alteração

--- App/src/components/header/index.tsx
+++ App/src/components/header/index.tsx
@@ -13,7 +13,7 @@ export function Header() {
 
         <View className="flex-row items-center justify-center gap-1">
           <Feather name="map-pin" size={14} color="#FF0000" />
-          <Text className="text-lg font-bold"> Campo grnade </Text>
+          <Text className="text-lg font-bold"> Nossa Senhora do Socorro </Text>
         </View>
       </View>
 
--- App/src/components/trending/index.tsx
+++ App/src/components/trending/index.tsx
@@ -15,21 +15,24 @@ export interface FoodProps {
 
 export function TrendingFoods() {
   const [foods, setFoods] = useState<FoodProps[]>([]);
+
   useEffect(() => {
     async function getFoods() {
-      const response = await fetch("http://10.0.1.198:3000/foods ");
+      const response = await fetch("http://10.0.1.198:3000/foods");
       const data = await response.json();
       setFoods(data);
     }
 
     getFoods();
   }, []);
+
   return (
     <FlatList
       data={foods}
       renderItem={({ item }) => <CardHorizontalFood food={item} />}
       horizontal={true}
-      contentContainerStyle={{ gap: 14 }}
+      contentContainerStyle={{ gap: 14, paddingLeft: 16, paddingRight: 16 }}
+      showsHorizontalScrollIndicator={false}
     />
   );
 }
